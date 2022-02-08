f23dfa98a0bc0e6299c203d3dfd71bc7a86019b4
mbelitz <mbelitz@ufl.edu>
2022-02-08 16:55:53 -0500

    init

---
+++ scripts/bird_lep_phenoComparison.R
@@ -0,0 +1,4 @@
+library(dplyr)
+
+birds <- readRDS("data/phenoEstimates/pheno-data-2020-08-25.rds")
+leps <- read.csv("data/phenoEstimates/adult_bfly_phenometrics_noCountCircles_withFull2020Data.csv")
