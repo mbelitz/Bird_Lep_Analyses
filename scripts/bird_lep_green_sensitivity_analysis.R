6e0fd5bd5097a10371f5a62895251c76f3cb7a93
mbelitz <mbelitz@ufl.edu>
2022-03-29 09:39:43 -0400

    3/28/22 update

--- scripts/bird_lep_green_sensitivity_analysis.R
+++ scripts/bird_lep_green_sensitivity_analysis.R
@@ -76,9 +76,49 @@ b
 
 nd <- data.frame(spring.dev = -152.5:137.5, year = mean(greenup$year), cell = 1)
 
-predict(t_gu,nd)
-
 greenup_sens <- t_gu@beta[2]
+
+## now predict green up across first a low spring.gdd
+length(unique(greenup$cell))
+gu_pdf_low <- data.frame(spring.dev = rep(mean(greenup$spring.dev) - sd(greenup$spring.dev), 73),
+                  year = rep(mean(greenup$year),73),
+                  cell = unique(greenup$cell))
+gu_pdf_mid <- data.frame(spring.dev = rep(mean(greenup$spring.dev), 73),
+                         year = rep(mean(greenup$year),73),
+                         cell = unique(greenup$cell))
+gu_pdf_high <- data.frame(spring.dev = rep(mean(greenup$spring.dev) + sd(greenup$spring.dev), 73),
+                         year = rep(mean(greenup$year),73),
+                         cell = unique(greenup$cell))
+
+gu_pred_low <- predictInterval(t_gu, newdata = gu_pdf_low, which = "full")
+gu_pred_mid <- predictInterval(t_gu, newdata = gu_pdf_mid, which = "full")
+gu_pred_high <- predictInterval(t_gu, newdata = gu_pdf_high, which = "full")
+
+gu_pred_low_df <- gu_pdf_low  %>% 
+  cbind(gu_pred_low)
+gu_pred_mid_df <- gu_pdf_mid %>% 
+  cbind(gu_pred_mid)
+gu_pred_high_df <- gu_pdf_high  %>% 
+  cbind(gu_pred_high)
+
+library(sf)
+hex_sf <- raster::shapefile("data/hex_grid_crop.shp") %>% 
+  st_as_sf()
+
+hex_sf <- st_transform(hex_sf, "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ")
+hex_sf$cell <- as.character(hex_sf$cell)
+
+gu_pred_high_sf <- left_join(hex_sf, gu_pred_high_df) %>% 
+  filter(!is.na(fit))
+gu_pred_mid_sf <- left_join(hex_sf, gu_pred_mid_df) %>% 
+  filter(!is.na(fit))
+gu_pred_low_sf <- left_join(hex_sf, gu_pred_low_df) %>% 
+  filter(!is.na(fit))
+
+ggplot() +
+  geom_sf(gu_pred_mid_sf, mapping = aes(fill = fit))
+ggplot() +
+  geom_sf(gu_pred_low_sf, mapping = aes(fill = fit))
 
 # let's do birds next
 arr_gdd_pc$cluster <- as.character(arr_gdd_pc$cluster)
@@ -136,8 +176,23 @@ arr_spring.dev <-    t_ar@beta[2]
 arr_spring.dev_c2 <- t_ar@beta[4]
 arr_spring.dev_c3 <- t_ar@beta[5]
 
-plot_model(t_ar, type = "pred", terms = c("year", "cluster"), ci.lvl = NA)
-plot_model(t_ar, type = "pred", terms = c("spring.dev", "cluster"), ci.lvl = NA)
+plot_model(t_ar, type = "eff", terms = c("year", "cluster"))
+plot_model(t_ar, type = "eff", terms = c("spring.dev", "cluster"))
+
+## now predict arrival across first a low spring.gdd
+source('scripts/functions_toPredictModels.R')
+
+ar_high_c1_sf <- generate_arrival_predicted_sf(lowmidhigh = 1, grouping = 1)
+ar_high_c2_sf <- generate_arrival_predicted_sf(lowmidhigh = 1, grouping = 2)
+ar_high_c3_sf <- generate_arrival_predicted_sf(lowmidhigh = 1, grouping = 3)
+
+ar_mid_c1_sf <- generate_arrival_predicted_sf(lowmidhigh = 0, grouping = 1)
+ar_mid_c2_sf <- generate_arrival_predicted_sf(lowmidhigh = 0, grouping = 2)
+ar_mid_c3_sf <- generate_arrival_predicted_sf(lowmidhigh = 0, grouping = 3)
+
+ar_low_c1_sf <- generate_arrival_predicted_sf(lowmidhigh = -1, grouping = 1)
+ar_low_c2_sf <- generate_arrival_predicted_sf(lowmidhigh = -1, grouping = 2)
+ar_low_c3_sf <- generate_arrival_predicted_sf(lowmidhigh = -1, grouping = 3)
 
 ## now let's do bird fledging
 fledge_gdd_pc$cluster <- as.character(fledge_gdd_pc$cluster)
@@ -165,7 +220,7 @@ sp_fledge <- lmer(mean_fledge ~ spring.gdd + year + spring.gdd:year +
 
 summary(sp_fledge)
 
-plot_model(sp_fledge, type = "pred", terms = c("year", "spring.gdd", "cluster"))
+plot_model(sp_fledge, type = "eff", terms = c("year", "spring.gdd", "cluster"))
 
 # deviation model 
 t_fledge <- lmer(mean_fledge ~ spring.dev + year + 
@@ -181,9 +236,21 @@ fledge_spring.dev <- t_fledge@beta[2]
 fledge_spring.dev_c2 <- t_fledge@beta[4]
 fledge_spring.dev_c3 <- t_fledge@beta[5]
 
-plot_model(t_fledge, type = "pred", terms = c("year", "cluster"), ci.lvl = NA)
-plot_model(t_fledge, type = "pred", terms = c("spring.dev", "cluster"), ci.lvl = NA)
+plot_model(t_fledge, type = "eff", terms = c("year", "cluster"))
+plot_model(t_fledge, type = "eff", terms = c("spring.dev", "cluster"))
 
+## get sf objects of fleding
+fledge_high_c1_sf <- generate_fledge_predicted_sf(lowmidhigh = 1, grouping = 1)
+fledge_high_c2_sf <- generate_fledge_predicted_sf(lowmidhigh = 1, grouping = 2)
+fledge_high_c3_sf <- generate_fledge_predicted_sf(lowmidhigh = 1, grouping = 3)
+
+fledge_mid_c1_sf <- generate_fledge_predicted_sf(lowmidhigh = 0, grouping = 1)
+fledge_mid_c2_sf <- generate_fledge_predicted_sf(lowmidhigh = 0, grouping = 2)
+fledge_mid_c3_sf <- generate_fledge_predicted_sf(lowmidhigh = 0, grouping = 3)
+
+fledge_low_c1_sf <- generate_fledge_predicted_sf(lowmidhigh = -1, grouping = 1)
+fledge_low_c2_sf <- generate_fledge_predicted_sf(lowmidhigh = -1, grouping = 2)
+fledge_low_c3_sf <- generate_fledge_predicted_sf(lowmidhigh = -1, grouping = 3)
 
 ## lep sensitivity time
 leps_gdd <- leps_gdd %>% 
@@ -218,19 +285,28 @@ t_leps <- lmer(q5 ~ spring.dev + year +
 
 summary(t_leps)
 
-lep_spring.dev <- t_leps@beta[2]
-lep_spring.dev_RL <- t_leps@beta[4]
-lep_spring.dev_RP <- t_leps@beta[5]
+plot_model(t_leps, type = "eff", terms = c("year", "code"))
+plot_model(t_leps, type = "eff", terms = c("spring.dev", "code")) 
 
+## generate sf objects for lep predictions
+leps_high_RL_sf <- generate_lep_predicted_sf(lowmidhigh = 1, grouping = "RL")
+leps_high_RP_sf <- generate_lep_predicted_sf(lowmidhigh = 1, grouping = "RP")
+leps_high_RE_sf <- generate_lep_predicted_sf(lowmidhigh = 1, grouping = "RE")
 
-plot_model(t_leps, type = "pred", terms = c("year", "code"), ci.lvl = NA)
-p <- plot_model(t_leps, type = "pred", terms = c("spring.dev", "code"), ci.lvl = NA) 
+leps_mid_RL_sf <- generate_lep_predicted_sf(lowmidhigh = 0, grouping = "RL")
+leps_mid_RP_sf <- generate_lep_predicted_sf(lowmidhigh = 0, grouping = "RP")
+leps_mid_RE_sf <- generate_lep_predicted_sf(lowmidhigh = 0, grouping = "RE")
 
+leps_low_RL_sf <- generate_lep_predicted_sf(lowmidhigh = -1, grouping = "RL")
+leps_low_RP_sf <- generate_lep_predicted_sf(lowmidhigh = -1, grouping = "RP")
+leps_low_RE_sf <- generate_lep_predicted_sf(lowmidhigh = -1, grouping = "RE")
 
 ## plot together
 library(ggpubr)
 
-arr_sens <- ggplot() +
+x = c(-100:100)
+
+ggplot() +
   geom_line(mapping = aes(x = x, y = greenup_sens*x, color = "Greenup"), 
             size = 1.1, linetype = "dashed") + 
   geom_line(mapping = aes(x = x, y = arr_spring.dev*x, color = "Arrival (C1)"), 
@@ -387,3 +463,366 @@ cp <- cowplot::plot_grid(green_sp_res, lep_sp_resid,
 
 cp
 ggsave(plot = cp, filename = "figures/spatial_residuals.png", width = 12, height = 8)
+
+## now we are plotting spatial locations of estimates in regards to greenup
+arr_fig_c1_high <- left_join(ar_high_c1_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c1_high_sf <- left_join(hex_sf, arr_fig_c1_high) %>% 
+  filter(!is.na(Difference))
+
+p1 <- ggplot() +
+  geom_sf(arr_fig_c1_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C1 Arrival, Warm year") +
+  theme_bw()
+
+arr_fig_c2_high <- left_join(ar_high_c2_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c2_high_sf <- left_join(hex_sf, arr_fig_c2_high) %>% 
+  filter(!is.na(Difference))
+
+p2 <- ggplot() +
+  geom_sf(arr_fig_c2_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C2 Arrival, Warm year") +
+  theme_bw()
+
+arr_fig_c3_high <- left_join(ar_high_c3_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c3_high_sf <- left_join(hex_sf, arr_fig_c3_high) %>% 
+  filter(!is.na(Difference))
+
+p3 <- ggplot() +
+  geom_sf(arr_fig_c3_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C3 Arrival, Warm year") +
+  theme_bw()
+
+# now move on to fledge
+fledge_fig_c1_high <- left_join(fledge_high_c1_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c1_high_sf <- left_join(hex_sf, fledge_fig_c1_high) %>% 
+  filter(!is.na(Difference))
+
+p4 <- ggplot() +
+  geom_sf(fledge_fig_c1_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C1 Fledge, Warm year") +
+  theme_bw()
+
+fledge_fig_c2_high <- left_join(fledge_high_c2_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c2_high_sf <- left_join(hex_sf, fledge_fig_c2_high) %>% 
+  filter(!is.na(Difference))
+
+p5 <- ggplot() +
+  geom_sf(fledge_fig_c2_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C2 Fledge, Warm year") +
+  theme_bw()
+
+fledge_fig_c3_high <- left_join(fledge_high_c3_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c3_high_sf <- left_join(hex_sf, fledge_fig_c3_high) %>% 
+  filter(!is.na(Difference))
+
+p6 <- ggplot() +
+  geom_sf(fledge_fig_c3_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C3 Fledge, Warm year") +
+  theme_bw()
+
+## moving on to leps
+leps_fig_RE_high <- left_join(leps_high_RE_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RE_high_sf <- left_join(hex_sf, leps_fig_RE_high) %>% 
+  filter(!is.na(Difference))%>% 
+  filter(Difference < 100)
+
+p7 <- ggplot() +
+  geom_sf(leps_fig_RE_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Egg, Warm year") +
+  theme_bw()
+
+leps_fig_RP_high <- left_join(leps_high_RP_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RP_high_sf <- left_join(hex_sf, leps_fig_RP_high) %>% 
+  filter(!is.na(Difference)) %>% 
+  filter(Difference < 100)
+
+p8 <- ggplot() +
+  geom_sf(leps_fig_RP_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Pupae, Warm year") +
+  theme_bw()
+
+leps_fig_RL_high <- left_join(leps_high_RL_sf, gu_pred_high_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RL_high_sf <- left_join(hex_sf, leps_fig_RL_high) %>% 
+  filter(!is.na(Difference))%>% 
+  filter(Difference < 100)
+
+p9 <- ggplot() +
+  geom_sf(leps_fig_RL_high_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Larvae, Warm year") +
+  theme_bw()
+
+library(cowplot)
+cp <- plot_grid(p1, p2, p3,
+                p4, p5, p6,
+                p7, p8, p9,
+                nrow = 3, ncol = 3)
+
+ggsave(filename = "figures/warmYear_difference.png", plot = cp,
+       width = 12, height = 12)
+
+
+
+
+## do spatial difference but for mid
+arr_fig_c1_mid <- left_join(ar_mid_c1_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c1_mid_sf <- left_join(hex_sf, arr_fig_c1_mid) %>% 
+  filter(!is.na(Difference))
+
+p1_mid <- ggplot() +
+  geom_sf(arr_fig_c1_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C1 Arrival, Average year") +
+  theme_bw()
+
+arr_fig_c2_mid <- left_join(ar_mid_c2_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c2_mid_sf <- left_join(hex_sf, arr_fig_c2_mid) %>% 
+  filter(!is.na(Difference))
+
+p2_mid <- ggplot() +
+  geom_sf(arr_fig_c2_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C2 Arrival, Average year") +
+  theme_bw()
+
+arr_fig_c3_mid <- left_join(ar_mid_c3_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c3_mid_sf <- left_join(hex_sf, arr_fig_c3_mid) %>% 
+  filter(!is.na(Difference))
+
+p3_mid <- ggplot() +
+  geom_sf(arr_fig_c3_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C3 Arrival, Average year") +
+  theme_bw()
+
+# now move on to fledge
+fledge_fig_c1_mid <- left_join(fledge_mid_c1_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c1_mid_sf <- left_join(hex_sf, fledge_fig_c1_mid) %>% 
+  filter(!is.na(Difference))
+
+p4_mid <- ggplot() +
+  geom_sf(fledge_fig_c1_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C1 Fledge, Average year") +
+  theme_bw()
+
+fledge_fig_c2_mid <- left_join(fledge_mid_c2_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c2_mid_sf <- left_join(hex_sf, fledge_fig_c2_mid) %>% 
+  filter(!is.na(Difference))
+
+p5_mid <- ggplot() +
+  geom_sf(fledge_fig_c2_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C2 Fledge, Average year") +
+  theme_bw()
+
+fledge_fig_c3_mid <- left_join(fledge_mid_c3_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c3_mid_sf <- left_join(hex_sf, fledge_fig_c3_mid) %>% 
+  filter(!is.na(Difference))
+
+p6_mid <- ggplot() +
+  geom_sf(fledge_fig_c3_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C3 Fledge, Average year") +
+  theme_bw()
+
+## moving on to leps
+leps_fig_RE_mid <- left_join(leps_mid_RE_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RE_mid_sf <- left_join(hex_sf, leps_fig_RE_mid) %>% 
+  filter(!is.na(Difference))%>% 
+  filter(Difference < 100)
+
+p7_mid <- ggplot() +
+  geom_sf(leps_fig_RE_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Egg, Average year") +
+  theme_bw()
+
+leps_fig_RP_mid <- left_join(leps_mid_RP_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RP_mid_sf <- left_join(hex_sf, leps_fig_RP_mid) %>% 
+  filter(!is.na(Difference)) %>% 
+  filter(Difference < 100)
+
+p8_mid <- ggplot() +
+  geom_sf(leps_fig_RP_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Pupae, Average year") +
+  theme_bw()
+
+leps_fig_RL_mid <- left_join(leps_mid_RL_sf, gu_pred_mid_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RL_mid_sf <- left_join(hex_sf, leps_fig_RL_mid) %>% 
+  filter(!is.na(Difference))%>% 
+  filter(Difference < 100)
+
+p9_mid <- ggplot() +
+  geom_sf(leps_fig_RL_mid_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Larvae, Average year") +
+  theme_bw()
+
+
+
+
+## do spatial difference but for low
+arr_fig_c1_low <- left_join(ar_low_c1_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c1_low_sf <- left_join(hex_sf, arr_fig_c1_low) %>% 
+  filter(!is.na(Difference))
+
+p1_low <- ggplot() +
+  geom_sf(arr_fig_c1_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C1 Arrival, Cold year") +
+  theme_bw()
+
+arr_fig_c2_low <- left_join(ar_low_c2_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c2_low_sf <- left_join(hex_sf, arr_fig_c2_low) %>% 
+  filter(!is.na(Difference))
+
+p2_low <- ggplot() +
+  geom_sf(arr_fig_c2_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C2 Arrival, Cold year") +
+  theme_bw()
+
+arr_fig_c3_low <- left_join(ar_low_c3_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+arr_fig_c3_low_sf <- left_join(hex_sf, arr_fig_c3_low) %>% 
+  filter(!is.na(Difference))
+
+p3_low <- ggplot() +
+  geom_sf(arr_fig_c3_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C3 Arrival, Cold year") +
+  theme_bw()
+
+# now move on to fledge
+fledge_fig_c1_low <- left_join(fledge_low_c1_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c1_low_sf <- left_join(hex_sf, fledge_fig_c1_low) %>% 
+  filter(!is.na(Difference))
+
+p4_low <- ggplot() +
+  geom_sf(fledge_fig_c1_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C1 Fledge, Cold year") +
+  theme_bw()
+
+fledge_fig_c2_low <- left_join(fledge_low_c2_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c2_low_sf <- left_join(hex_sf, fledge_fig_c2_low) %>% 
+  filter(!is.na(Difference))
+
+p5_low <- ggplot() +
+  geom_sf(fledge_fig_c2_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C2 Fledge, Cold year") +
+  theme_bw()
+
+fledge_fig_c3_low <- left_join(fledge_low_c3_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+fledge_fig_c3_low_sf <- left_join(hex_sf, fledge_fig_c3_low) %>% 
+  filter(!is.na(Difference))
+
+p6_low <- ggplot() +
+  geom_sf(fledge_fig_c3_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("C3 Fledge, Cold year") +
+  theme_bw()
+
+## moving on to leps
+leps_fig_RE_low <- left_join(leps_low_RE_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RE_low_sf <- left_join(hex_sf, leps_fig_RE_low) %>% 
+  filter(!is.na(Difference))%>% 
+  filter(Difference < 100)
+
+p7_low <- ggplot() +
+  geom_sf(leps_fig_RE_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Egg, Cold year") +
+  theme_bw()
+
+leps_fig_RP_low <- left_join(leps_low_RP_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RP_low_sf <- left_join(hex_sf, leps_fig_RP_low) %>% 
+  filter(!is.na(Difference)) %>% 
+  filter(Difference < 100)
+
+p8_low <- ggplot() +
+  geom_sf(leps_fig_RP_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Pupae, Cold year") +
+  theme_bw()
+
+leps_fig_RL_low <- left_join(leps_low_RL_sf, gu_pred_low_df, by = "cell") %>% 
+  mutate(Difference = fit.x - fit.y) 
+leps_fig_RL_low_sf <- left_join(hex_sf, leps_fig_RL_low) %>% 
+  filter(!is.na(Difference))%>% 
+  filter(Difference < 100)
+
+p9_low <- ggplot() +
+  geom_sf(leps_fig_RL_low_sf, mapping = aes(fill = Difference)) +
+  scale_fill_gradient2() +
+  ggtitle("Larvae, Cold year") +
+  theme_bw()
+
+
+library(cowplot)
+cp <- plot_grid(p1_low, p1_mid, p1,
+                p4_low, p4_mid, p4,
+                nrow = 2, ncol = 3)
+
+ggsave(filename = "figures/bird_C1_difference.png", plot = cp,
+       width = 12, height = 8)
+
+
+cp <- plot_grid(p2_low, p2_mid, p2,
+                p5_low, p5_mid, p5,
+                nrow = 2, ncol = 3)
+
+ggsave(filename = "figures/bird_C2_difference.png", plot = cp,
+       width = 12, height = 8)
+
+
+cp <- plot_grid(p3_low, p3_mid, p3,
+                p6_low, p6_mid, p6,
+                nrow = 2, ncol = 3)
+
+ggsave(filename = "figures/bird_C3_difference.png", plot = cp,
+       width = 12, height = 8)
+
+cp <- plot_grid(p7_low, p7_mid, p7,
+                p8_low, p8_mid, p8,
+                p9_low, p9_mid, p9,
+                nrow = 3, ncol = 3)
+
+ggsave(filename = "figures/leps_difference.png", plot = cp,
+       width = 12, height = 8)
